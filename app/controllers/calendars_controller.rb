3e866b0e89091ad47d74444ee72e4cfb8306b697
naokey-convoy <convoy@katch.ne.jp>
2026-06-20 21:22:52 +0900

    Issue4~6の解消

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,19 +34,12 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-<<<<<<< HEAD
+
       days = { 
                month: (@todays_date + x).month, 
                date: (@todays_date+x).day,
                wday: wdays[(@todays_date + x).wday],
                plans: today_plans
-=======
-
-      days = { :month => (@todays_date + x).month, 
-               :date => (@todays_date+x).day,
-               :wday => wdays[(@todays_date + x).wday],
-               :plans => today_plans
->>>>>>> 7c46dab376bf82ef3ec2116a056286218562d70a
               }
 
 
