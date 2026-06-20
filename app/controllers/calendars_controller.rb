32583edf0a75d6f5cc7022d7630c241467e3c48f
naokey-convoy <convoy@katch.ne.jp>
2026-06-20 21:18:32 +0900

    Reapply "Merge pull request #8 from naokey-convoy/second_training"
    This reverts commit fe334b1c67464134a5a3d3fa9290cea3f068469b.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def get_week
@@ -34,10 +34,25 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = {month: (@todays_date + x).month, date:  (@todays_date+x).day, plans:  today_plans}
+<<<<<<< HEAD
+      days = { 
+               month: (@todays_date + x).month, 
+               date: (@todays_date+x).day,
+               wday: wdays[(@todays_date + x).wday],
+               plans: today_plans
+=======
+
+      days = { :month => (@todays_date + x).month, 
+               :date => (@todays_date+x).day,
+               :wday => wdays[(@todays_date + x).wday],
+               :plans => today_plans
+>>>>>>> 7c46dab376bf82ef3ec2116a056286218562d70a
+              }
+
+
       @week_days.push(days)
-    end
+    end 
 
   end
 end
-0
+
